76fdf1258fc53872b59489437378ff050f1391c4
Leon Magee <leonmagee@hotmail.com>
2017-05-07 10:59:12 -0700

    update

--- lib/rest-api-endpoints.php
+++ lib/rest-api-endpoints.php
@@ -10,20 +10,20 @@ add_action( 'rest_api_init', 'cr_register_rest_endpoint' );
  */
 function cr_register_rest_endpoint() {
 
-	register_rest_route( 'quizian', '/sports/basketball/nba', array(
+	register_rest_route( 'quizian', '/sports/basketball/nba/(?P<num>\d+)', array(
 		'methods'  => 'GET',
 		'callback' => 'levon_rest_api_sports_bb_nba'
 	) );
 }
 
-function levon_rest_api_sports_bb_nba() {
+function levon_rest_api_sports_bb_nba($param) {
 	/**
 	 * Get Professional Basketball Data
 	 */
 	global $wpdb;
 
 	$table = $wpdb->prefix . 'quizian';
-	$query = "SELECT `question`, `correct`, `incorrect_1`, `incorrect_2`, `incorrect_3` from {$table} WHERE `sub_sub_cat` = 'nba'";
+	$query = "SELECT `question`, `correct`, `incorrect_1`, `incorrect_2`, `incorrect_3` from {$table} WHERE `sub_sub_cat` = 'nba' ORDER BY RAND() LIMIT {$param['num']}";
 	$data  = $wpdb->get_results( $query );
 
 	$data_array = [];
