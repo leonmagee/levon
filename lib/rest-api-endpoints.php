dc03983f0b2862cbc769da4528b080cc307f58fe
Leon Magee <leonmagee@hotmail.com>
2017-05-25 14:51:33 -0700

    update

--- lib/rest-api-endpoints.php
+++ lib/rest-api-endpoints.php
@@ -30,16 +30,82 @@ function levon_rest_api_questions() {
 	$query = "SELECT `question`, `correct`, `incorrect_1`, `incorrect_2`, `incorrect_3` from {$table} WHERE `cat` = 'history'";
 	$data  = $wpdb->get_results( $query );
 
-	$data_array = [];
+	$data_array_history = [];
 	foreach ( $data as $item ) {
-		$data_array[] = array(
+		$data_array_history[] = array(
 			'q' => $item->question,
 			'c' => $item->correct,
 			'i' => array( $item->incorrect_1, $item->incorrect_2, $item->incorrect_3 ),
 		);
 	}
 
+	//return $data_array_history;
+
+	$query = "SELECT `question`, `correct`, `incorrect_1`, `incorrect_2`, `incorrect_3` from {$table} WHERE `cat` = 'sports'";
+	$data  = $wpdb->get_results( $query );
+
+	$data_array_sports = [];
+	foreach ( $data as $item ) {
+		$data_array_sports[] = array(
+			'q' => $item->question,
+			'c' => $item->correct,
+			'i' => array( $item->incorrect_1, $item->incorrect_2, $item->incorrect_3 ),
+		);
+	}
+
+
+	$query = "SELECT `question`, `correct`, `incorrect_1`, `incorrect_2`, `incorrect_3` from {$table} WHERE `cat` = 'geography'";
+	$data  = $wpdb->get_results( $query );
+
+	$data_array_geography = [];
+	foreach ( $data as $item ) {
+		$data_array_geography[] = array(
+			'q' => $item->question,
+			'c' => $item->correct,
+			'i' => array( $item->incorrect_1, $item->incorrect_2, $item->incorrect_3 ),
+		);
+	}
+
+	$query = "SELECT `question`, `correct`, `incorrect_1`, `incorrect_2`, `incorrect_3` from {$table} WHERE `cat` = 'music'";
+	$data  = $wpdb->get_results( $query );
+
+	$data_array_music = [];
+	foreach ( $data as $item ) {
+		$data_array_music[] = array(
+			'q' => $item->question,
+			'c' => $item->correct,
+			'i' => array( $item->incorrect_1, $item->incorrect_2, $item->incorrect_3 ),
+		);
+	}
+
+	$query = "SELECT `question`, `correct`, `incorrect_1`, `incorrect_2`, `incorrect_3` from {$table} WHERE `cat` = 'entertainment'";
+	$data  = $wpdb->get_results( $query );
+
+	$data_array_entertainment= [];
+	foreach ( $data as $item ) {
+		$data_array_entertainment[] = array(
+			'q' => $item->question,
+			'c' => $item->correct,
+			'i' => array( $item->incorrect_1, $item->incorrect_2, $item->incorrect_3 ),
+		);
+	}
+
+	//return $data_array_sports;
+
+	$data_array[] = array(
+		'history' => $data_array_history,
+		'sports' => $data_array_sports,
+		'geography' => $data_array_geography,
+		'music' => $data_array_music,
+		'entertainment' => $data_array_entertainment,
+	);
+
 	return $data_array;
+
+
+
+
+
 }
 
 function levon_rest_api_sports_bb_nba($param) {
