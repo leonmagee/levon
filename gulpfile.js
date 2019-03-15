009af855d17db4accb24b8958d18ff4786369c63
Leon Magee <leonmagee33@gmail.com>
2019-03-15 10:55:24 -0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
-/**
- *  Initialize Gulp
- */
-var gulp = require('gulp');
+const { task, watch, series, parallel, src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const minifycss = require('gulp-clean-css');
+const log = require('fancy-log');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 
-/**
- *  Load Gulp Dependencies
- */
-var sass = require('gulp-sass');
-var minifycss = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var log = require('fancy-log');
-var concat = require('gulp-concat');
-var browserSync = require('browser-sync').create();
+const scss_src = 'assets/scss/**/*.scss';
+const css_dest = 'assets/css';
+const css_name = 'main.min.css';
+const scss_message = '⚡️ 🦕 ⚡️ 🦕 ⚡️ 🦕 ⚡️ 🦕 ⚡️  SCSS has compiled! ⚡️ 🦕 ⚡️ 🦕 ⚡️ 🦕 ⚡️ 🦕 ⚡️'
 
-// chalk colors: red | blue | yellow | green | cyan | magenta | white
+//const js_src = 'assets/js/scripts.js';
 
-/**
- *  I need to figure out how to get live reload working... this is such a PITA...
- *  I can either switch to browser sync or else try the new method outlined in the video...
- */
 
-//gulp.task('default', 'scss');
+function watcher() {
+    watch(scss_src, css);
+}
 
-gulp.task('scss', function () {
-    gulp.src([
+function js() {
+    return true;
+}
+
+function css() {
+    return src([
         'assets/css/normalize.css',
         'assets/css/style.css', 
         'assets/css/flaticon.css', 
@@ -32,54 +31,109 @@ gulp.task('scss', function () {
         'assets/scss/import.scss'
         ])
         .pipe(sass({style: 'compressed', errLogToConsole: true}))
-        .pipe(concat('main.min.css'))
+        .pipe(concat(css_name))
         .pipe(minifycss())
-        .pipe(gulp.dest('assets/css'))
-        .pipe(browserSync.stream());
-    log('Compiled!');
-});
+        .pipe(dest(css_dest))
+        //.pipe(browserSync.stream())
+        .pipe(browserSync.reload({stream:true}))
+        .on('end', function(){ log(scss_message); });
+    //return log('Compiled!');
+}
 
-// gulp.task('browser-sync', function() {
+function callBrowserSync() {
+    return browserSync.init({
+        proxy: "https://levon.test", // this proxys my dev site to localhost:{port}
+        open: false,
+        port: 9999,
+        https: {
+            key: "/Users/leonmagee/.localhost-ssl/localhost.key",
+            cert: "/Users/leonmagee/.localhost-ssl/localhost.crt"
+        }
+    });
+}
+
+task('css', css);
+
+task('callBrowserSync', callBrowserSync);
+
+const build = parallel(callBrowserSync, css, watcher);
+
+task('default', build);
+
+
+
+
+
+
+
+// function scss() {
+//     gulp.src([
+//         'assets/css/normalize.css',
+//         'assets/css/style.css', 
+//         'assets/css/flaticon.css', 
+//         'assets/css/fontello.css', 
+//         'assets/css/fontawesome-all.css', 
+//         'assets/scss/import.scss'
+//         ])
+//         .pipe(sass({style: 'compressed', errLogToConsole: true}))
+//         .pipe(concat('main.min.css'))
+//         .pipe(minifycss())
+//         .pipe(gulp.dest('assets/css'))
+//         //.pipe(browserSync.stream());
+//         .pipe(browserSync.reload({stream:true}));
+//     log('Compiled!');
+// }
+
+// function callBrowserSync() {
 //     browserSync.init({
-//         proxy: "https://levon.test", // this proxys my dev site to localhost:3000
+//         proxy: "https://levon.test", // this proxys my dev site to localhost:{port}
 //         open: false,
 //         port: 9999,
 //         https: {
-//             key: "/Users/leonmagee/.localhost-ssl/key.pem",
-//             cert: "/Users/leonmagee/.localhost-ssl/cert.pem"
+//             key: "/Users/leonmagee/.localhost-ssl/localhost.key",
+//             cert: "/Users/leonmagee/.localhost-ssl/localhost.crt"
 //         }
 //     });
-// });
+// }
 
-// gulp.task('watch', function () {
+// function watch() {
 
 //     /**
 //      *  Watch PHP files for changes
 //      */
 //     var php = '**/*.php';
-
 //     gulp.watch(php).on('change', function (file) {
-
-//         gulp.src(php).pipe(browserSync.stream());
-
+//         gulp.src(php).pipe(browserSync.reload({stream:true}));
 //         log('[ ' + file.path + ' ]');
 //     });
 
 //     var js = 'assets/js/**/*.js';
-
 //     gulp.watch(js).on('change', function (file) {
-
-//         gulp.src(js).pipe(browserSync.stream());
-
+//         gulp.src(js).pipe(browserSync.reload({stream:true}));
 //         log('[ ' + file.path + ' ]');
 //     });
 
 //     /**
-//      *  Watch SCSS files for changes - trigger 'scss' task
+//      *  Watch SCSS files for changes - trigger 'scss' function
 //      */
-//     gulp.watch('assets/scss/**/*.scss', ['scss']);
-// });
+//     gulp.watch('assets/scss/**/*.scss', function() {
+//         //console.log('watching?');
+//         return scss;
+//     });
+//         // Endless stream mode
+//     // return watch('assets/scss/**/*.scss', { ignoreInitial: false })
+//     //         .pipe(gulp.dest('build'));
+// }
 
-//gulp.task('default', ['scss', 'watch', 'browser-sync']);
-gulp.task('default', ['scss']);
+// // gulp.task('callback', function () {
+// //     // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+// //     return watch('css/**/*.css', function () {
+// //         gulp.src('css/**/*.css')
+// //             .pipe(gulp.dest('build'));
+// //     });
+// // });
 
+// //var build = gulp.series(callBrowserSync, scss, watch);
+// var build = gulp.series(watch);
+
+// gulp.task('default', build);
---
+++ gulpfileTest.js
@@ -0,0 +1,127 @@
+// /**
+//  *  Initialize Gulp
+//  */
+// var gulp = require('gulp');
+
+// /**
+//  *  Load Gulp Dependencies
+//  */
+// var sass = require('gulp-sass');
+// var minifycss = require('gulp-clean-css');
+// //var rename = require('gulp-rename');
+// var log = require('fancy-log');
+// var concat = require('gulp-concat');
+// var browserSync = require('browser-sync').create();
+
+// function scss() {
+//     gulp.src([
+//         'assets/css/normalize.css',
+//         'assets/css/style.css', 
+//         'assets/css/flaticon.css', 
+//         'assets/css/fontello.css', 
+//         'assets/css/fontawesome-all.css', 
+//         'assets/scss/import.scss'
+//         ])
+//         .pipe(sass({style: 'compressed', errLogToConsole: true}))
+//         .pipe(concat('main.min.css'))
+//         .pipe(minifycss())
+//         .pipe(gulp.dest('assets/css'))
+//         .pipe(browserSync.stream());
+//     log('Compiled!');
+// }
+
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    // browserSync.init({
+    //     server: "./app"
+    // });
+    browserSync.init({
+        proxy: "https://levon.test", // this proxys my dev site to localhost:{port}
+        open: false,
+        port: 9999,
+        https: {
+            key: "/Users/leonmagee/.localhost-ssl/localhost.key",
+            cert: "/Users/leonmagee/.localhost-ssl/localhost.crt"
+        }
+    });
+
+    gulp.watch("assets/scss/**/*.scss", ['sass']);
+    //gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    //hreturn gulp.src("app/scss/*.scss")
+    return gulp.src("assets/scss/**/*.scss")
+        .pipe(sass())
+        .pipe(sass({style: 'compressed', errLogToConsole: true}))
+        .pipe(concat('main.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
+
+
+
+
+
+// function callBrowserSync() {
+//     browserSync.init({
+//         proxy: "https://levon.test", // this proxys my dev site to localhost:{port}
+//         open: false,
+//         port: 9999,
+//         https: {
+//             key: "/Users/leonmagee/.localhost-ssl/localhost.key",
+//             cert: "/Users/leonmagee/.localhost-ssl/localhost.crt"
+//         }
+//     });
+// }
+
+// function watch() {
+
+//     /**
+//      *  Watch PHP files for changes
+//      */
+//     var php = '**/*.php';
+//     gulp.watch(php).on('change', function (file) {
+//         gulp.src(php).pipe(browserSync.stream());
+//         log('[ ' + file.path + ' ]');
+//     });
+
+//     var js = 'assets/js/**/*.js';
+//     gulp.watch(js).on('change', function (file) {
+//         gulp.src(js).pipe(browserSync.stream());
+//         log('[ ' + file.path + ' ]');
+//     });
+
+//     /**
+//      *  Watch SCSS files for changes - trigger 'scss' function
+//      */
+//     gulp.watch('assets/scss/**/*.scss', function() {
+//         console.log('watching?');
+//         return scss;
+//     });
+//         // Endless stream mode
+//     // return watch('assets/scss/**/*.scss', { ignoreInitial: false })
+//     //         .pipe(gulp.dest('build'));
+// }
+
+// gulp.task('callback', function () {
+//     // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+//     return watch('css/**/*.css', function () {
+//         gulp.src('css/**/*.css')
+//             .pipe(gulp.dest('build'));
+//     });
+// });
+
+// var build = gulp.series(callBrowserSync, scss, watch);
+
+// gulp.task('default', build);
