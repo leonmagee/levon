6d519ce6466303ed3b7e6cb6bf075700d800d698
Leon Magee <leon@comic-con.org>
2020-03-06 10:06:19 -0800

    update

--- lib/projects_loop.php
+++ lib/projects_loop.php
@@ -1,27 +1,28 @@
 <div class="projects-wrap">
 	<?php
-	/**
-	 * Projects Section
-	 */
-	$args           = array( 'post_type' => 'project' );
-	$projects_query = new WP_Query( $args );
-	while ( $projects_query->have_posts() ) {
-		$projects_query->the_post();
-		$gallery = get_field( 'gallery' );
-		if ( has_post_thumbnail() ) {
-			$background_image = get_the_post_thumbnail_url();
-		} else {
-			$background_image = $gallery[0]['sizes']['large'];
-		}
-		if ( $gallery || has_post_thumbnail() ) {
-			?>
+/**
+ * Projects Section
+ */
+$args = array('post_type' => 'project');
+$projects_query = new WP_Query($args);
+while ($projects_query->have_posts()) {
+    $projects_query->the_post();
+    $gallery = get_field('gallery');
+    if (has_post_thumbnail()) {
+        $background_image = get_the_post_thumbnail_url();
+    } else {
+        $background_image = $gallery[0]['sizes']['large'];
+    }
+    if ($gallery || has_post_thumbnail()) {
+        ?>
 			<div class="project-item-wrap" style="background-image: url('<?php echo $background_image; ?>');">
-				<a href="<?php the_permalink(); ?>">
+				<a href="<?php the_permalink();?>">
 					<div class="hover-shade">
 						<div class="project-item-circle">View Project</div>
 					</div>
-				</a>
+        </a>
+        <div class="project-title-new"><?php the_title();?> / <?php echo get_field('sub_title'); ?></div>
 			</div>
-		<?php } ?>
-	<?php } ?>
+		<?php }?>
+	<?php }?>
 </div>
