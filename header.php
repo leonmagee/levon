608e43b8a06c3715d714dd7e08868eec64150b7e
Leon Magee <leonmagee33@gmail.com>
2021-09-25 15:13:25 -0700

    update

--- header.php
+++ header.php
@@ -39,8 +39,8 @@
 			<!-- nav -->
 			<nav class="nav" role="navigation">
 				<?php //html5blank_nav(); //doesn't work anymore ?>
-				<?php wp_nav_menu();?>
-			</nav>
+				<?php wp_nav_menu(['menu' => 1]);?>
+			</na1>
 			<!-- /nav -->
 
 		</div>
